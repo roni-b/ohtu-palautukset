bfee2d4479168a9aaadeac6e6c392ea514435b35
Roni Bärman <104189902+roni-b@users.noreply.github.com>
2023-11-27 20:25:50 +0200

    update

--- viikko5/int-joukko/src/tests/int_joukko_test.py
+++ viikko5/int-joukko/src/tests/int_joukko_test.py
@@ -22,20 +22,20 @@ class TestIntJoukko(unittest.TestCase):
         for luku in lisattavat:
             joukko.lisaa(luku)
 
-        self.assertEqual(joukko.mahtavuus(), 14)
+        self.assertEqual(joukko.alkioiden_maara(), 14)
         self.assertTrue(joukko.kuuluu(11))
         joukko.poista(11)
         self.assertFalse(joukko.kuuluu(11))
-        self.assertEqual(joukko.mahtavuus(), 13)
+        self.assertEqual(joukko.alkioiden_maara(), 13)
 
     def test_lukuja_lisatty_maara(self):
         self.joukko.lisaa(4)
-        self.assertEqual(self.joukko.mahtavuus(), 3)
+        self.assertEqual(self.joukko.alkioiden_maara(), 3)
 
     def test_sama_luku_menee_joukkoon_vaan_kerran(self):
         self.joukko.lisaa(10)
         self.joukko.lisaa(3)
-        self.assertEqual(self.joukko.mahtavuus(), 2)
+        self.assertEqual(self.joukko.alkioiden_maara(), 2)
 
     def test_vain_lisatyt_luvut_loytyvat(self):
         self.assertTrue(self.joukko.kuuluu(10))
@@ -45,7 +45,7 @@ class TestIntJoukko(unittest.TestCase):
     def test_poistettu_ei_ole_enaa_joukossa(self):
         self.joukko.poista(3)
         self.assertFalse(self.joukko.kuuluu(3))
-        self.assertEqual(self.joukko.mahtavuus(), 1)
+        self.assertEqual(self.joukko.alkioiden_maara(), 1)
 
     def test_palautetaan_oikea_taulukko(self):
         odotettu = [3, 55, 99]
