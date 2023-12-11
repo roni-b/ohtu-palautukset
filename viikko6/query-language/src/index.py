7e46b4a386368d2562253fad494aef61e83f4abd
Roni Bärman <104189902+roni-b@users.noreply.github.com>
2023-12-11 23:51:30 +0200

    add

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -8,22 +8,33 @@ def main():
     stats = Statistics(reader)
 
     query = QueryBuilder()
-    m1 = (
-        query
-        .playsIn("NYR")
-        .hasAtLeast(10, "goals")
-        .hasFewerThan(20, "goals")
+    # m1 = (
+    #     query
+    #     .playsIn("NYR")
+    #     .hasAtLeast(10, "goals")
+    #     .hasFewerThan(20, "goals")
+    #     .build()
+    # )
+
+    # m2 = (
+    #     query
+    #     .playsIn("EDM")
+    #     .hasAtLeast(50, "points")
+    #     .build()
+    # )
+
+    # matcher = query.oneOf(m1, m2)
+    matcher = (
+    query
+        .oneOf(
+        query.playsIn("PHI")
+            .hasAtLeast(10, "assists")
+            .hasFewerThan(5, "goals"),
+        query.playsIn("EDM")
+            .hasAtLeast(50, "points")
+        )
         .build()
     )
-
-    m2 = (
-        query
-        .playsIn("EDM")
-        .hasAtLeast(50, "points")
-        .build()
-    )
-
-    matcher = query.oneOf(m1, m2).build()
 
     for player in stats.matches(matcher):
         print(player)
